9265db247ade42a83784a09e3f1ce646a76edf3f
Diego F. Asanza <f.asanza@gmail.com>
2015-07-14 20:43:56 +0200

    adding dmm task'

---
+++ src/app/dmmtask.c
@@ -0,0 +1,31 @@
+/*
+ * dmmtask.c
+ *
+ * Copyright (c) 2015, Diego F. Asanza. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ *
+ * Created on 3. Juni 2015, 01:12
+ */
+
+#include <systasks.h>
+
+
+
+//extern dmm_task_t* AvailableTasks + 3 = &t;
+//AvailableTasks = &t;
+
+//REGISTER_TASK(&t);
