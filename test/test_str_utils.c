9c113711831d36869c265beb7b58a75ce3307395
Diego F. Asanza <f.asanza@gmail.com>
2016-02-19 22:18:40 +0100

    add .gitlab-ci.yml file

--- test/test_str_utils.c
+++ test/test_str_utils.c
@@ -19,7 +19,7 @@ void test_strutils(void){
   TEST_ASSERT_EQUAL_DOUBLE(5.18305611836e-3, x);
   char buffer[10];
   utils_dtostr(buffer, 9, x);
-  TEST_ASSERT_EQUAL_STRING("0.00518306", buffer);
+  TEST_ASSERT_EQUAL_STRING("0.00518305", buffer);
   utils_dtostr(buffer, 5, 2.342345e2);
   TEST_ASSERT_EQUAL_STRING("234.23", buffer);
   utils_dtostr(buffer, 5, -2.342345e2);
