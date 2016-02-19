ced45a3e13e409155a9d1d6b2209f762007edecf
Diego F. Asanza <f.asanza@gmail.com>
2016-02-19 22:16:02 +0100

    add .gitlab-ci.yml file

--- test/simulation/sim_test_fixture.rb
+++ test/simulation/sim_test_fixture.rb
@@ -1,8 +1,14 @@
-OUT_FILE = "test/simulation/out.txt"
-File.delete OUT_FILE if File.exists? OUT_FILE 
-IO.popen("mdb.sh test/simulation/sim_instructions.txt > test/simulation/simlog.txt")
+require 'rbconfig'
+is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+OUT_FILE = "./test/simulation/out.txt"
+File.delete OUT_FILE if File.exists? OUT_FILE
+if is_windows
+	var = IO.popen("mdb.bat ./test/simulation/sim_instructions.txt > ./test/simulation/simlog.txt")
+else
+	var = IO.popen("mdb.sh ./test/simulation/sim_instructions.txt > ./test/simulation/simlog.txt")
+end
 Process.wait
-if File.exists? OUT_FILE 
+if File.exists? OUT_FILE
     file_contents = File.read OUT_FILE
     file_contents.gsub!("\r\n","\n")
     print file_contents
